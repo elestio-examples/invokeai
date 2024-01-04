7a4f1d7e2f7de3d4d8cb3445846051a3e4366c79
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-01-04 13:29:24 +0200

    first

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# invokeai
-Deploy InvokeAI on Elestio with CI/CD
+# InvokeAI CI/CD pipeline
+
+<a href="https://dash.elest.io/deploy?source=cicd&social=dockerCompose&url=https://github.com/elestio-examples/invokeai"><img src="deploy-on-elestio.png" alt="Deploy on Elest.io" width="180px" /></a>
+
+Deploy InvokeAI with CI/CD on Elestio
+
+<img src="invokeai.png" style='width: 100%;'/>
+<br/>
+<br/>
+
+# Once deployed ...
+
+You can open InvokeAI here:
+
+    URL: https://[CI_CD_DOMAIN]
+    login: root
+    password: [ADMIN_PASSWORD]
---
+++ scripts/preInstall.sh
@@ -0,0 +1,17 @@
+#set env vars
+set -o allexport; source .env; set +o allexport;
+
+lshw_output=$(lshw)
+
+if [[ $lshw_output == *nvidia* ]]; then
+    echo "Installing the GPU version..."
+    sed -i 's/#deploy:/deploy:/g' ./docker-compose.yml
+    sed -i 's/#  resources:/  resources:/g' ./docker-compose.yml
+    sed -i 's/#    reservations:/    reservations:/g' ./docker-compose.yml
+    sed -i 's/#      devices:/      devices:/g' ./docker-compose.yml
+    sed -i 's/#        - driver: nvidia/        - driver: nvidia/g' ./docker-compose.yml
+    sed -i 's/#          count: all/          count: all/g' ./docker-compose.yml
+    sed -i 's/#          capabilities: \[gpu\]/          capabilities: \[gpu\]/g' ./docker-compose.yml
+else
+    echo "Installing the CPU version..."
+fi
